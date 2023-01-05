2b62e7d9f448d298efc6eb7abdd991ea52e20f62
Prasanth <prasanth.iosdev@gmail.com>
2023-01-05 18:21:48 +0530

    Updated link for 3Sum

--- 12_3Sum_Day5.playground/Contents.swift
+++ 12_3Sum_Day5.playground/Contents.swift
@@ -1,6 +1,6 @@
 //MARK: - 3Sum
 
-// Link: https://leetcode.com/problems/longest-palindromic-substring/
+// Link: https://leetcode.com/problems/3sum/
 // Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
 // Notice that the solution set must not contain duplicate triplets.
 
